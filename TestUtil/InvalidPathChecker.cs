1687361260858bed662871d296c52d3acdd09547
David Allison <62114487+david-allison-1@users.noreply.github.com>
2021-06-12 20:16:08 +0100

    feat: validate file paths (#611)
    * Rename Ned Beg files
    
    Contained ";"
    
    * feat: validate file paths
    
    We had a few cases where valid Mac paths are invalid in Windows
    
    Fixes #609

---
+++ TestUtil/InvalidPathChecker.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Manx_Search_Data.TestUtil
+{
+    public static class InvalidPathChecker
+    {
+        public static bool IsValid(string path)
+        {
+            // This isn't perfect, but we're not protecting against malicious uses.
+            // Invalid on Windows
+            var invalidCharacters = "<>:;\"|?*".Select(c => c).ToHashSet();
+
+            var pathToTest = path.Substring(4); //C:// should be trimmed - matches :
+
+            return pathToTest.All(c => !invalidCharacters.Contains(c)) && !GetParentName(path).TrimEnd().EndsWith('.');
+        }
+
+        private static string GetParentName(string path)
+        {
+            // Directory.GetParent() normalizes the path
+            return path.Substring(0, path.LastIndexOfAny(new[] { '\\', '/' }));
+        }
+    }
+}
