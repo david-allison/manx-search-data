23a3b3d80350fa6bae069744fb963aed7ac7cb30
David Allison <62114487+david-allison@users.noreply.github.com>
2022-11-30 23:00:42 +0000

    fix: validate 'Manx Original'/'English Original" column
    Occasionally called Original X, in which it isn't handled

--- TestUtil/DocumentLineMap.cs
+++ TestUtil/DocumentLineMap.cs
@@ -1,15 +1,29 @@
-﻿using CsvHelper.Configuration;
+﻿using System;
+using CsvHelper.Configuration;
 
 namespace Manx_Search_Data.TestUtil
 {
     public class DocumentLineMap : ClassMap<DocumentLine>
     {
+        private readonly string[] _invalidFieldNames = { "Original Manx", "Original English" };
         public DocumentLineMap()
         {
             Map(m => m.English);
             Map(m => m.Manx);
-            Map(m => m.Notes).Optional();
             Map(m => m.Page).Optional();
+            Map(m => m.Notes).Optional().Convert(args =>
+            {
+                // arbitrary validation of fields
+                foreach (var invalidFieldName in _invalidFieldNames)
+                {
+                    if (args.Row.TryGetField<string>(invalidFieldName, out _))
+                    {
+                        throw new ArgumentException($"Invalid Field: ${invalidFieldName}. Should be 'X Original'");
+                    }
+                }
+                args.Row.TryGetField("Notes", out string notes);
+                return notes;
+            });
         }
     }
 }
