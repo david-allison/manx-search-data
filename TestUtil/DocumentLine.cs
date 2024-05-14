b4be2c629cb0b650aa47486fd114ad95a48f29a1
lauterb <47546076+lauterb@users.noreply.github.com>
2024-05-14 18:13:55 -0400

    add new fields, remove header testing from Notes area since now done elsewhere

--- TestUtil/DocumentLine.cs
+++ TestUtil/DocumentLine.cs
@@ -4,6 +4,9 @@
     {
         public string English { get; set; }
         public string Manx { get; set; }
+        public double? SubStart { get; set; }
+        public double? SubEnd { get; set; }
+        public string? Speaker { get; set; }
         public string Notes { get; set; }
         public int? Page { get; set; }
     }
