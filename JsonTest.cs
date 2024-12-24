c948f8e4539663509bf84e5029b3cad0b3e1aa6d
David Allison <62114487+david-allison@users.noreply.github.com>
2024-12-24 20:53:13 +0000

    lint: format manifest.json.txt
    using provided unit test
    
    Python did not open a number of files
    
    Issue 1291

---
+++ JsonTest.cs
@@ -0,0 +1,37 @@
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using Manx_Search_Data.TestData;
+using Manx_Search_Data.TestUtil;
+using NUnit.Framework;
+
+namespace Manx_Search_Data;
+
+[TestFixture]
+public class JsonTest
+{        
+    [DatapointSource]
+    // ReSharper disable once UnusedMember.Global
+    public OpenSourceDocument[] AllDocuments = Documents.AllDocuments.OfType<OpenSourceDocument>().ToArray();
+
+    [Theory]
+    [Ignore("Trailing commas")]
+    public void CsvFileIsUtf8(OpenSourceDocument document)
+    {
+        var path = document.LocationOnDisk + "/manifest.json.txt";
+
+        var input = File.ReadAllText(path);
+        
+        var output = JsonSerializer.Serialize(
+            JsonSerializer.Deserialize<dynamic>(input), 
+            new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                WriteIndented = true,
+            }
+        );
+        
+        File.WriteAllText(path, output);
+        Assert.AreEqual(input, output);
+    }
+}
